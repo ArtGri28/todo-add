f45a3b25e302ed733d758734736623527df1fdb9
Art.Gri <artem.grishan@mail.ru>
2025-11-17 14:39:12 +0300

    обавил код и новые файлы

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+	todo "todo-app"
+	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error ocoured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+
+}
